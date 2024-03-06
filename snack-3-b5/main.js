7a8e876deb17a22ab7facad2b8bd9c24cbaa4c6f
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-03-06 12:50:27 +0100

    fix on last fix

--- snack-3-b5/main.js
+++ snack-3-b5/main.js
@@ -1,6 +1,6 @@
 /* snack3-b5
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
 
-function reverse(string){
+function reverseString(string){
     return string.split("").reverse().join("");
 }
